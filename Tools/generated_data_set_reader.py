bca450f1208fbe6acbac69bc835a0962961c8769
Amin <mohammad-amin.haji-bagheri-fard.1@ulaval.ca>
2017-10-06 15:03:12 -0400

    06-10-2017_3:03_ pre & post processing

--- Tools/generated_data_set_reader.py
+++ Tools/generated_data_set_reader.py
@@ -1,12 +1,13 @@
-def generated_data_set_reader (**kwargs):
-    input_data_set = kwargs["extracted_data_set"]
-    conversion_methods = kwargs["conversion_methods"]
+def generated_data_set_reader (extracted_data_set, conversion_methods, **kwargs):
+    input_data_set = kwargs("extracted_data_set")
+    conversion_methods = kconversion_methods
 
     data_bank = []
     for device_data_set in input_data_set:
         bursts_of_a_single_device = device_data_set["a_Single_Device"]
 
         burst_finger_print = []
+        burst_index = 0
         for burst in bursts_of_a_single_device:
             for subregion_index in range(len(burst)):
                 current_subregion = burst[subregion_index]
@@ -17,5 +18,8 @@ def generated_data_set_reader (**kwargs):
                         for methods in conversion_methods:
                             current_subregion[key] = methods (current_subregion[key])
 
-    return
+            burst_finger_print [burst_index]
+            burst_index += 1
+
+    return data_bank
 
