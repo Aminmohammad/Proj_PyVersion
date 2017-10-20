29feaea3ab36cb1386ec1f77bfcdf121c991a566
Amin <mohammad-amin.haji-bagheri-fard.1@ulaval.ca>
2017-10-20 15:01:10 -0400

    20-10-2017_02:57 pm Selection of InputDataCollection is Graphical

--- preFeatureGenerationProcessor/preProcessingTools/dwt_calculator/dwt_calculator.py
+++ preFeatureGenerationProcessor/preProcessingTools/dwt_calculator/dwt_calculator.py
@@ -2,6 +2,5 @@ from  pywt import dwt
 
 
 def dwt_calculator (signal):
-    # approximation_coefficients, detail_coefficients = dwt(signal, 'haar')
-    print("ALkjalkslakjsl")
-    #return detail_coefficients
+    approximation_coefficients, detail_coefficients = dwt(signal, 'haar')
+    return detail_coefficients
